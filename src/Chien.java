05d626f6f5d17049a141187acb0c7b2ffdabcae2
spacewalker96 <hmiyedhassan@gmail.com>
2020-12-07 16:42:02 +0100

    Added Scanner EX 1

--- src/Chien.java
+++ src/Chien.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
+public class Chien {
+    public String name ;
+    Scanner sc = new Scanner(System.in);
+     String aboyer() {
+         System.out.println("lorsque aboyer :  ");
+            return sc.nextLine();
+    }
+    String manger() {
+        System.out.println("lorsque manger :  ");
+        return sc.nextLine();
+    }
+
+    public static void main(String[] args) {
+        Chien ch1 = new Chien();
+        System.out.println("Call Rex");
+        System.out.println(ch1.name="Rex "+"dit "+ch1.aboyer()+" "+ ch1.manger());
+        System.out.println("Call Max");
+        System.out.println(ch1.name="Max "+"dit "+ch1.aboyer()+" "+ ch1.manger());
+    }
+}
+
+//without Scanner
+/*
 public class Chien {
     public String name ;
     static String aboyer() {
@@ -13,3 +38,4 @@ public class Chien {
         System.out.println(ch1.name="Max"+ch1.aboyer()+ ch1.manger());
     }
 }
+ */
